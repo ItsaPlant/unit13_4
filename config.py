bca369ef7174d6fbfc7816e8e7c706c9ccbc7ef2
ItsaPlant <krzysiekmichalik6@gmail.com>
2021-10-05 22:35:56 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "remember-to-add-secret-key"
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get('DATABASE_URL') or
+        'sqlite:///' + os.path.join(BASE_DIR, 'mikroblog.db')
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
